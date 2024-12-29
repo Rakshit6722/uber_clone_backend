fedd00dacff05aa4fc624b8bd88426aae00f0b5b
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-29 16:47:18 +0530

    [Rakshit] Refactor: config cookie-parser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { connectDB } = require("./config/database");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 dotenv.config();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5050;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
