e62a8458367ad2a157dbed6816b2e5211589febd
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-29 15:31:05 +0530

    [Rakshit] Add: user login controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,3 +23,27 @@ exports.registerUser = async (req, res) => {
 
     res.status(201).json({message:"User created successfully", user, token});
 }
+
+exports.loginUser = async(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({message:"Invalid email or password"});
+    }
+
+    const isPasswordMatch = await user.comparePassword(password);
+
+    if(!isPasswordMatch){
+        return res.status(401).json({message:"Invalid email or password"});
+    }
+
+    const token = user.generateAuthToken();
+    res.status(200).json({message:"Login successful", user, token});
+}
