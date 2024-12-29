2cf4bb54532962c58402aea4ea05f7bdfe060eb9
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-29 17:12:06 +0530

    [Rakshit] Refactor: add black list token check

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,11 +2,17 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const blackListTokenModel = require("../models/blacklistToken.model");
 
 exports.authUser = async(req,res,next) => {
-    const token = req.cookies.token || req.header('Authorization').split(' ')[1] ;
+    const token = req.cookies.token || req.header('Authorization')?.split(' ')[1] ;
     if(!token){
-        return res.status(401).json({message:"Unauthorized",error:error.message});
+        return res.status(401).json({message:"Unauthorized"});
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({token});
+    if(isBlacklisted){
+        return res.status(401).json({message:"Unauthorized"});
     }
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
