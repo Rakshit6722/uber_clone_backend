154f1ae65dfb1d59448461262f8e415f081da714
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-29 16:40:51 +0530

    [Rakshit] Add: user get profile route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body } = require('express-validator')
-import { authUser } from "../middlewares/auth.middleware";
+const { authUser } = require("../middlewares/auth.middleware");
 const userController = require("../controllers/user.controller");
 
 router.post('/register', [
@@ -15,7 +15,7 @@ router.post('/login',[
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 8 characters long'),
 ], userController.loginUser);
 
-router.get('/profile', authUser,userController.getUserProfile);
+router.get('/profile', authUser, userController.getUserProfile);
 
 module.exports = router;
 
