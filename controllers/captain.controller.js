c8e7559f49392b65c86b53dfd8ac249463d9ce68
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-29 19:57:25 +0530

    [Rakshit] Refactor: add try catch in register captain controller

--- controllers/captain.controller.js
+++ controllers/captain.controller.js
@@ -1,29 +1,33 @@
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 
-exports.registerCaptain = async(req,res) => {
+exports.registerCaptain = async (req, res) => {
     const error = validationResult(req);
-    if(!error.isEmpty()){
-        return res.status(400).json({errors:error.array()});
+    if (!error.isEmpty()) {
+        return res.status(400).json({ errors: error.array() });
     }
 
-    const {fullname,email,password,vehicle} = req.body;
+    const { fullname, email, password, vehicle } = req.body;
 
     const hashedPassword = await captainModel.hashPassword(password);
 
-    const captain = await captainService.createCaptain({
-        firstname:fullname.firstname,
-        lastname:fullname.lastname,
-        email,
-        password:hashedPassword,
-        color:vehicle.color,
-        plate:vehicle.plate,
-        capacity:vehicle.capacity,
-        vehicleType:vehicle.vehicleType
-    });
+    try {
+        const captain = await captainService.createCaptain({
+            firstname: fullname.firstname,
+            lastname: fullname.lastname,
+            email,
+            password: hashedPassword,
+            color: vehicle.color,
+            plate: vehicle.plate,
+            capacity: vehicle.capacity,
+            vehicleType: vehicle.vehicleType
+        });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 
     const token = await captain.generateToken();
 
-    res.status(201).json({message:"Captain registered successfully",captain,token});
+    res.status(201).json({ message: "Captain registered successfully", captain, token });
 }
