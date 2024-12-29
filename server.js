a5e19badff84e77c2c28be0b2b2e810dd8f096a7
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-29 15:19:50 +0530

    [Rakshit] Add: config user routes in server

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { connectDB } = require("./config/database");
 const cors = require("cors");
+const userRoutes = require("./routes/user.routes");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5050;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
