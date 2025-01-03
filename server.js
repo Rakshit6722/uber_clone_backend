8e0250501b2e1ed81991888f8d432633bd189f8e
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-03 21:39:25 +0530

    [Rakshit] Add: config cors

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 5050;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
